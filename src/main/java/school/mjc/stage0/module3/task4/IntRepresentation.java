1ab9ff1cf476c90b197121834133ee836ae7412c
Marcos <104943037+MarcosSSA011@users.noreply.github.com>
2024-02-09 18:21:59 -0600

    m3 t4 1

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+    	char c = 99;
+    	char h = 104;
+    	char a = 97;
+    	char r = 114;
+    	System.out.println(c + "\n" + h + "\n" + a + "\n" + r);
     }
 }
